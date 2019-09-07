9b23c130f06d63ae1edfeff807aedcfda01428ef
Neo <ksssugar@gmail.com>
2019-09-07 21:26:44 +0800

    Added SearchBar component and GET request to the YouTube API

---
+++ src/components/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import youtube from '../api/youtube';
+import SearchBar from './SearchBar';
+
+
+class App extends React.Component {
+  state = { videos: [] };
+
+ 
+  onTermSubmit = async term => {
+    const response = await youtube.get('/search', {
+      params: {
+        q: term
+      }
+    });
+
+    console.log(response)
+  };
+
+  render() {
+    return (
+      <div className="ui container" style={{ marginTop: '10px' }}>
+        <SearchBar onTermSubmit={this.onTermSubmit} />
+      </div>
+    ) 
+  };
+};
+
+export default App;
