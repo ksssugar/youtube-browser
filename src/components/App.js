f53928d1868804079288c5558ba263893d093292
Neo <ksssugar@gmail.com>
2019-09-10 02:22:01 +0800

    Added more components

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import youtube from '../api/youtube';
 import SearchBar from './SearchBar';
+import VideoList from './VideoList';
+import VideoDetails from './VideoDetails';
 
 
 class App extends React.Component {
-  state = { videos: [] };
+  state = { videos: [], selectedVideo: null };
 
- 
+  componentDidMount(video) {
+    this.onTermSubmit('saitama');
+  }
+
   onTermSubmit = async term => {
     const response = await youtube.get('/search', {
       params: {
@@ -14,13 +19,30 @@ class App extends React.Component {
       }
     });
 
-    console.log(response)
+    this.setState({ 
+      videos: response.data.items,
+      selectedVideo: response.data.items[0]
+    })
   };
+
+  onVideoSelect = (video) => {
+    this.setState({ selectedVideo: video })
+  }
 
   render() {
     return (
       <div className="ui container" style={{ marginTop: '10px' }}>
         <SearchBar onTermSubmit={this.onTermSubmit} />
+        <div className="ui grid">
+          <div className="ui row">
+            <div className="eleven wide column">
+              <VideoDetails video={this.state.selectedVideo} />
+            </div>
+            <div className="five wide column">
+              <VideoList videos={this.state.videos} onVideoSelect={this.onVideoSelect} />
+            </div>
+          </div>
+        </div>
       </div>
     ) 
   };
---
+++ src/components/VideoCard.jsx
@@ -0,0 +1,15 @@
+import './VideoCard.css';
+import React from 'react';
+
+const VideoCard = ({ video, onVideoSelect }) => {
+    return (
+        <div className="video-card item" onClick={()=> onVideoSelect(video)}>
+            <img className="ui image" src={video.snippet.thumbnails.medium.url} alt={video.snippet.title} />
+            <div className="content">
+                <div className="header">{video.snippet.title}</div>
+            </div>
+        </div>
+    );
+};
+
+export default VideoCard;
